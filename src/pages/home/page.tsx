36f9ef89d6f5f84f4575a74865c8a66bba37d977
Reinaldo Guedes <rguedes.dev@gmail.com>
2026-05-16 07:25:21 -0300

    Inseir algumas molduras para a foto de perfil, falta ajustar.

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
-import ProfilePicture from "../../assets/kon.jpg";
-import AnimeAds from "../../assets/ads-anime.jpg";
-
+// Componentes
+import { PictureFrame } from "../../shared/components/PictureFrame/page";
 import { Input } from "../../shared/components/Input";
+
+// Icones
+import { TbPhoneCall } from "react-icons/tb";
+import { AiOutlineVideoCamera } from "react-icons/ai";
+import { MdOutlinePersonAddAlt } from "react-icons/md";
+import { MdOutlineGroups } from "react-icons/md";
+import { ImMakeGroup } from "react-icons/im";
+
+// Imagens
+import AnimeAds from "../../assets/ads-anime.jpg";
 
 function HomePage() {
   const [status, setStatus] = useState("ausente");
@@ -29,28 +38,31 @@ function HomePage() {
 
   return (
     <main className="h-screen flex flex-col bg-[#ebf3f6]">
-      <aside className="flex flex-row gap-2 bg-white my-3 mx-4 p-4 rounded-md shadow-md">
-        <div className="border-4 border-solid border-green-600 p-1 rounded-xl">
-          <img
-            className="rounded-md"
-            src={ProfilePicture}
-            alt="Profile Picture"
-            width="100"
-            height="100"
-          />
-        </div>
-        <div>
-          <div className="flex flex-row items-center gap-2">
-            <span
-              className={`w-3 h-3 rounded-full border-[2px] border-solid border-zinc-600 ${
-                statusConfig[status as keyof typeof statusConfig].color
-              }`}
-            />
-            <span className="text-[15px] font-semibold">Kon-sama ZS</span>
-            <span className="text-[15px] font-light italic">{`(Ausente)`}</span>
-          </div>
+      <aside className="flex flex-col gap-2 bg-white my-3 mx-4 p-4 rounded-md shadow-md">
+        <div className="flex flex-row gap-2">
+          <PictureFrame />
+          <div className="flex flex-col justify-between">
+            <div className="flex flex-col">
+              <div className="flex flex-row items-center gap-2">
+                <span
+                  className={`w-3 h-3 rounded-full border-[2px] border-solid border-zinc-600 ${
+                    statusConfig[status as keyof typeof statusConfig].color
+                  }`}
+                />
+                <span className="text-[15px] font-semibold">Kon-sama ZS</span>
+                <span className="text-[15px] font-light italic">{`(Ausente)`}</span>
+              </div>
 
-          <p className="text-[13px] text-[#6ebea1]">{`<Insira uma mensagem pessoal>`}</p>
+              <p className="text-[13px] text-[#6ebea1]">{`<Insira uma mensagem pessoal>`}</p>
+            </div>
+
+            <div className="flex flex-row items-center gap-3">
+              <MdOutlinePersonAddAlt size={23} />
+              <ImMakeGroup size={15} />
+              <TbPhoneCall size={20} />
+              <AiOutlineVideoCamera size={20} />
+            </div>
+          </div>
         </div>
       </aside>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        thunderFlicker: {
+          "0%, 100%": { opacity: "0.2", filter: "blur(4px)" },
+          "5%, 15%, 25%": {
+            opacity: "1",
+            filter: "blur(2px)",
+          } /* Clarão do raio */,
+          "10%, 20%": { opacity: "0.4", filter: "blur(5px)" },
+          "30%, 85%": {
+            opacity: "0.1",
+            filter: "blur(6px)",
+          } /* Tempo de espera entre raios */,
+          "88%, 92%": { opacity: "1", filter: "blur(1px)" } /* Segundo raio */,
+        },
+        gradientMove: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "300% 50%" },
+        },
+      },
+    },
   },
   plugins: [],
 };
